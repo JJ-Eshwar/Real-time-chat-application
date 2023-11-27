8aa73cf8e09c31ccfe214ce5abf0c4586eb21b75
JJ-Eshwar <jjeshwar776@gmail.com>
2023-11-27 19:20:37 +0530

    Updated the styling part

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser')
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Socket.io
 io.on("connection", (socket) => {
@@ -14,10 +16,18 @@ io.on("connection", (socket) => {
   });
 });
 
+
+messages = []
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
+});
+app.post("/", (req, res) => {
+  messages.push(req.body.messages)
+
+
+
 });
 
 server.listen(9000, () => console.log(`Server Started at PORT:9000`));
